b14ab953257637e0e014d184824a49d06aae31e2
Nick <nick.rattanaton@gmail.com>
2024-05-04 14:44:42 +0700

    ➕ Add scopus example

---
+++ src/web_scrape.py
@@ -0,0 +1,107 @@
+import os
+
+# import requests
+import logging
+
+# import json
+from pythonjsonlogger import jsonlogger
+from dotenv import load_dotenv
+from elsapy.elsclient import ElsClient
+from elsapy.elsprofile import ElsAuthor, ElsAffil
+from elsapy.elsdoc import FullDoc, AbsDoc
+from elsapy.elssearch import ElsSearch
+
+# from bs4 import BeautifulSoup
+
+load_dotenv()  # take environment variables from .env.
+logger = logging.getLogger()
+logHandler = logging.StreamHandler()
+formatter = jsonlogger.JsonFormatter()
+logHandler.setFormatter(formatter)
+logger.addHandler(logHandler)
+
+API_KEY = os.getenv("ELSEVIER_API_KEY")
+TIME_OUT = 10
+
+## Initialize client
+client = ElsClient(API_KEY)
+
+
+def read_affiliations(affil_id):
+    """Find affiliations by ID
+
+    Args:
+        affil_id (_type_): affiliation with ID as string
+    """
+    my_aff = ElsAffil(affil_id=affil_id)
+    if my_aff.read(client):
+        print("my_aff.name: ", my_aff.name)
+        my_aff.write()
+    else:
+        print("Read affiliation failed.")
+
+
+def read_scopus_abstract(scp_id):
+    """read scopus through scp_id
+
+    Args:
+        scp_id (string): scopus id
+    """
+    ## Scopus (Abstract) document example
+    # Initialize document with ID as integer
+    scp_doc = AbsDoc(scp_id=scp_id)
+    if scp_doc.read(client):
+        print("scp_doc.title: ", scp_doc.title)
+        scp_doc.write()
+    else:
+        print("Read document failed.")
+
+
+def read_science_direct_pii(sd_pii):
+    ## ScienceDirect (full-text) document example using PII
+    pii_doc = FullDoc(sd_pii=sd_pii)
+    if pii_doc.read(client):
+        print("pii_doc.title: ", pii_doc.title)
+        pii_doc.write()
+    else:
+        print("Read document failed.")
+
+
+def read_science_direct_doi(doi):
+    ## ScienceDirect (full-text) document example using DOI
+    doi_doc = FullDoc(doi=doi)
+    if doi_doc.read(client):
+        print("doi_doc.title: ", doi_doc.title)
+        doi_doc.write()
+    else:
+        print("Read document failed.")
+
+
+def search_scopus(auth_name):
+    """search scopus for author
+
+    Args:
+        auth_name (_type_): _description_
+    """
+    doc_srch = ElsSearch(
+        "AFFIL(dartmouth) AND AUTHOR-NAME(lewis) AND PUBYEAR > 2023", "scopus"
+    )
+    doc_srch.execute(client, get_all=False, count=1)
+    print("doc_srch has", len(doc_srch.results), "results.")
+    print(doc_srch.results[0])
+
+
+def main():
+    # ? Search something
+    search_scopus("keuskamp")
+    # ? Using this to be a core map old data 1-1
+    # read_scopus_abstract("85170238281")
+
+    # read_affiliations("60091507")
+    # read_science_direct_doi("10.1016/j.ijbiomac.2023.126316")
+    # read_science_direct_pii("S1674927814000082")
+    return
+
+
+if __name__ == "__main__":
+    main()
